9397f4db41ef383af62d0894a58e55c895c91ed8
William Janssen <janssenwilliam02@gmail.com>
2026-02-28 13:53:07 +0100

    solution for useToggle

--- SOLUTIONS/Phase_4/trial-3/src/hooks/useToggle.js
+++ SOLUTIONS/Phase_4/trial-3/src/hooks/useToggle.js
@@ -1,15 +1,20 @@
 // import useState
+import { useState } from "react";
 
 // Custom hook for boolean toggle logic
 // Demonstrates extracting simple but reusable patterns
 const useToggle = (initialValue = false) => {
   // Declare state variable 'value' with initial value: initialValue
+  const [value, setValue] = useState(initialValue);
 
   // Complete the function called toggle, that flips the boolean using the functional updater form
-  const toggle = () => {};
+  const toggle = () => {
+    setValue((prev) => !prev);
+  };
 
   // return the current value and the toggle method
   // syntax: return [____, ____];
+  return [value, toggle];
 };
 
 export default useToggle;
