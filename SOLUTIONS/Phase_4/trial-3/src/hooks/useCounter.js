203950594366814ddc0a71db74ff706fe61cfd84
William Janssen <janssenwilliam02@gmail.com>
2026-02-28 13:42:14 +0100

    completed useCount for solutions

--- SOLUTIONS/Phase_4/trial-3/src/hooks/useCounter.js
+++ SOLUTIONS/Phase_4/trial-3/src/hooks/useCounter.js
@@ -1,20 +1,29 @@
 // import useState
+import { useState } from "react";
 
 // Custom hook for counter logic with multiple operations
 // Shows how custom hooks can expose multiple functions
 const useCounter = (initialValue = 0) => {
   // Declare state variable 'count' with initial value: initialValue
+  const [count, setCount] = useState(initialValue);
 
   // Complete the increment function (adds 1), use the functional updater form
-  const increment = () => {};
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
 
   // Complete the decrement function (subtracts 1), use the functional updater form
-  const decrement = () => {};
+  const decrement = () => {
+    setCount((prev) => prev - 1);
+  };
 
   // Complete the reset function (resets count to initialValue)
-  const reset = () => {};
+  const reset = () => {
+    setCount(initialValue);
+  };
 
   // Return object with value and all functions: count, increment, decrement, reset
+  return { count, increment, decrement, reset };
 };
 
 export default useCounter;
