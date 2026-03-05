07f80c06f9f62eb21c92ba39b9b5936a1d21cfb9
William Janssen <janssenwilliam02@gmail.com>
2026-03-05 17:12:17 +0100

    add the context, provider and hooks for trial folder

---
+++ TRIALS/Phase_4/trial-4/src/context/ThemeContext.js
@@ -0,0 +1,4 @@
+// import createContext
+
+// Create and export context called ThemeContext, initialise it with null
+// syntax: export const variableName = ...
---
+++ TRIALS/Phase_4/trial-4/src/context/UserProvider.jsx
@@ -0,0 +1,33 @@
+// Import useState
+// Import UserContext
+
+// Provider component for user data
+// Destruct children from props and specify it in the param
+export function UserProvider({ children }) {
+  // Declare state variable user with a initial value of: {name: "", role: "viewer",}
+
+  // Create login function that updates user 
+  const login = (name, role) => {
+   
+  };
+
+  // Create logout function that resets user name and role to empty strings
+  const logout = () => {
+
+  };
+ 
+  // Create value object that holds user, and the methods login and logout
+  const value = {
+    ____,
+    ____,
+    ____,
+  };
+
+  // Return Provider with value and children
+  // syntax, UserContext.Provider, hold the value object, and wraps the children prop
+  return (
+    <____.Provider value={____}>
+      {____}
+    </____>
+  );
+}
